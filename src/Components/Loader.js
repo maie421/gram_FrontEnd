45f575d81d18777ef282765410a11a2a6b97162f
윤영미 <35258834+maie421@users.noreply.github.com>
2019-10-10 00:31:31 +0900

    search

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -16,7 +16,9 @@ const Animation = keyframes`
 
 const Loader = styled.div`
   animation: ${Animation} 1s linear infinite;
-`;
+  width: 100%;
+  text-align: center;
+  `;
 
 export default () => (
   <Loader>
---
+++ src/Routes/Search/SearchContainer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {withRouter} from "react-router-dom";
+import SearchPresenter from "./SearchPresenter";
+import { SEARCH } from "./SearchQueries";
+import {useQuery} from "react-apollo-hooks";
+
+/*export default withRouter(props => {
+    console.log(props);
+    return null;
+});*/
+
+export default withRouter(({location:{search}})=>{
+    const term =search.split("=")[1];
+    const {data,loading}=useQuery(SEARCH,{
+        skip:term===undefined,
+        variables:{
+            term
+        }
+    });
+    console.log(data);
+    return <SearchPresenter searchTerm={term} loading={loading} data={data}></SearchPresenter>;
+});
